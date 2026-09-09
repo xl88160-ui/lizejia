267f82d531ec3cbccc177510e0c5fdb73b4fc1be
xl88160-ui <xl88160@gmail.com>
2026-09-09 19:29:53 +0800

    Fix Supabase Storage endpoint

--- resources.js
+++ resources.js
@@ -1,5 +1,5 @@
 (()=>{
-const U='https://uvmojhlpmrpjpfcupwbk',K='sb_publishable_4GCp4gl6N91d_J9z_VuooA_JVNYZCVG',B='class-resources';
+const U='https://uvmojhlpmrpjpfcupwbk.supabase.co',K='sb_publishable_4GCp4gl6N91d_J9z_VuooA_JVNYZCVG',B='class-resources';
 const H={apikey:K,Authorization:'Bearer '+K};
 const esc=x=>String(x??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const fmt=n=>{if(n<1024)return n+' B';if(n<1048576)return (n/1024).toFixed(1)+' KB';return (n/1048576).toFixed(1)+' MB'};
